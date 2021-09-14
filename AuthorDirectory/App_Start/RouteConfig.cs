16aeba8eb671b745a194690b43a914a4dd7052b5
Tanjilur Rahman Nabil <nabil.odesk@gmail.com>
2021-09-14 09:22:50 +0600

    first commit

--- AuthorDirectory/App_Start/RouteConfig.cs
+++ AuthorDirectory/App_Start/RouteConfig.cs
@@ -15,8 +15,8 @@ namespace AuthorDirectory
 
             routes.MapRoute(
                 name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                url: "{controller}/{action}",
+                defaults: new { controller = "Author", action = "Authors" }
             );
         }
     }
