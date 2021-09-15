b09efa44f9a85d517a2d645c2a38080c5e602dd8
TanjilurNabil <nabil.ajkerdeal@gmail.com>
2021-09-15 14:08:16 +0600

    Remove favorite

---
+++ AuthorDirectory/Scripts/Script.js
@@ -0,0 +1,14 @@
+﻿var app = angular.module("storageModule", [
+    "ngAnimate",
+    "ngSanitize",
+    "ui.bootstrap",
+    "ngStorage"
+]);
+app.controller("storageController", function (
+    $scope,
+    $http,
+    $localStorage,
+    $window
+) {
+    $localStorage.data = [];
+});
